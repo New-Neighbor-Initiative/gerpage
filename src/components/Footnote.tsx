2abd254fad232e57e5dd570f032b372fb1c0deeb
Jargalan <jargalan0713@gmail.com>
2026-01-19 12:50:16 -0500

    footnote file add

---
+++ src/components/Footnote.tsx
@@ -0,0 +1,13 @@
+import "./Footnote.css"
+
+function Footnote(){
+    
+    return(
+        <div id="footnote-section">
+            <img src="/img/logo.png" alt="NNI logo" />
+        </div>
+    )
+    
+}
+
+export default Footnote;
