614dfc2dc202a6be16072d77838557c95138201c
NNI <nnidcengineering@gmail.com>
2026-01-20 22:25:17 -0500

    Contact Us with SVG details around

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,37 +1,43 @@
 import "./Contact.css"
 import { useEffect, useState } from "react";
 import { fetch as apiFetch } from "../api/api";
+import cornerDetail from "../assets/top_left_corner_detail.svg";
 
-function Contact(){
+function Contact() {
     const [contact, setContact] = useState<any>(null);
     useEffect(() => {
         apiFetch("/data/contact.json")
-          .then(res => res.json())
-          .then(data => setContact(data))
-          .catch(() => console.error("Failed to load about data"));
-      }, []);
+            .then(res => res.json())
+            .then(data => setContact(data))
+            .catch(() => console.error("Failed to load about data"));
+    }, []);
 
     if (!contact) return <p>Loading...</p>;
-    return(
+    return (
         <div className="contact-section" id="contact-section">
             <div id="top-section">
                 <div className="contact-title">
                     <h2>CONTACT US</h2>
                     <hr />
                 </div>
-                <input type="text" name="name" placeholder="Your Name" className = "contact-inputs" required/>
-                <input type="email" name="email" placeholder="Your Email" className = "contact-inputs" required/>
-                <textarea name="" placeholder = "Your Message" className = "contact-inputs" id = "message-area" required></textarea>
-                <button type = "submit" className = "contact-button">SEND</button>
+
+                <div className="contact-card">
+                    <img src={cornerDetail} className="corner-detail top-left" alt="" />
+                    <img src={cornerDetail} className="corner-detail top-right" alt="" />
+                    <img src={cornerDetail} className="corner-detail bottom-right" alt="" />
+                    <img src={cornerDetail} className="corner-detail bottom-left" alt="" />
+
+                    <input type="text" name="name" placeholder="Your Name" className="contact-inputs" required />
+                    <input type="email" name="email" placeholder="Your Email" className="contact-inputs" required />
+                    <textarea name="" placeholder="Your Message" className="contact-inputs" id="message-area" required></textarea>
+                    <button type="submit" className="contact-button">SEND</button>
+                </div>
             </div>
             <div className="footnote-section">
                 <img src="https://drive.google.com/drive/u/0/folders/1zXwKehlfIlkOZCNajgZ4PKXNq6U1mTJD" alt="logo" />
             </div>
         </div>
-        
-
     )
-
 }
 
 export default Contact;
