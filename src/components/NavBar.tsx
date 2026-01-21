89c372ffd42a9a1ba6785f3a97de1947cce979c3
NNI <nnidcengineering@gmail.com>
2026-01-20 22:26:03 -0500

    NavBar logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,45 +1,42 @@
+import logo from "../assets/logo.svg";
 import "./NavBar.css"
 
-function NavBar(){
+function NavBar() {
     const scrollTo = (id: string) => {
         const el = document.getElementById(id);
         if (el) {
-          el.scrollIntoView({ behavior: "smooth" });
+            el.scrollIntoView({ behavior: "smooth" });
         }
     };
-    return(
+    return (
         <>
-        <nav className="navbar">
-            <div className="logo">
-                <a href="/Home">
-                <img src="/img/logo.png" alt="NNI logo" className="logo-img" />
-                </a>
-            </div>
+            <nav className="navbar">
+                <div className="logo">
+                    <a href="gerpage/Home">
+                        <img src={logo} alt="NNI logo" className="logo-img" />
+                    </a>
+                </div>
 
-            <div className="right">
-                <nav className="navbar-inner">
-                    <button className="navbar-link" onClick={() => scrollTo("home-section")}>
-                    Home
-                    </button>
+                <div className="right">
+                    <nav className="navbar-inner">
+                        <button className="navbar-link" onClick={() => scrollTo("home-section")}>
+                            Home
+                        </button>
 
-                    <button className="navbar-link" onClick={() => scrollTo("about-section")}>
-                    About Us
-                    </button>
+                        <button className="navbar-link" onClick={() => scrollTo("about-section")}>
+                            About Us
+                        </button>
 
-                    <button className="navbar-link" onClick={() => scrollTo("team-section")}>
-                    Team
-                    </button>
+                        <button className="navbar-link" onClick={() => scrollTo("team-section")}>
+                            Team
+                        </button>
 
-                    <button className="navbar-link" onClick={() => scrollTo("event-section")}>
-                    Events
-                    </button>
-                    
-                    <button className="navbar-link" onClick={() => scrollTo("contact-section")}>
-                    Contact
-                    </button>
-                </nav>
-            </div>
-        </nav>
+                        <button className="navbar-link" onClick={() => scrollTo("contact-section")}>
+                            Contact
+                        </button>
+                    </nav>
+                </div>
+            </nav>
 
         </>
     )
