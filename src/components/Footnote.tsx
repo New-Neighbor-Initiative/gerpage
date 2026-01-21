9525c4f3ad1673250f4f005bc6ed15ff4009793c
NNI <nnidcengineering@gmail.com>
2026-01-20 22:56:13 -0500

    Footer and global smooth scrolling

--- src/components/Footnote.tsx
+++ src/components/Footnote.tsx
@@ -1,12 +1,23 @@
 import "./Footnote.css"
-import footerDetail from "../assets/footer_full_width_detail.svg";
+
 
 function Footnote() {
 
     return (
-        <div id="footnote-section">
-            <img src={footerDetail} alt="Footer Detail" className="footer-detail" />
-        </div>
+        <footer id="footnote-section">
+            <div className="footer-content">
+                <div className="footer-links">
+                    <a href="#home-section">Home</a>
+                    <a href="#schedule-section">Events</a>
+                    <a href="#about-section">About</a>
+                    <a href="#team-section">Team</a>
+                    <a href="#contact-section">Contact</a>
+                </div>
+                <div className="footer-info">
+                    <p>&copy; {new Date().getFullYear()} New Neighbor Initiative. All Rights Reserved.</p>
+                </div>
+            </div>
+        </footer>
     )
 
 }
